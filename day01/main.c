e94876e45a24f919304aa9c503b1de5580ca1a55
Brayden Zee <github@blueish.dev>
2025-12-01 00:36:05 -0500

    Day 1, part 2
    I am out of practice, this shouldn't have taken me this long

--- day01/main.c
+++ day01/main.c
@@ -1,26 +1,34 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 int main() {
     char direction;
     int amount;
 
     int current_pos = 50;
+    bool prev_was_0 = false;
 
     int count = 0;
 
     while (scanf("%c%d\n", &direction, &amount) == 2) {
         if (direction == 'L') {
             current_pos -= amount;
-            current_pos += 100;
-            current_pos %= 100;
         } else {
             current_pos += amount;
-            current_pos %= 100;
         }
 
-        if (current_pos == 0) {
+        count += abs(current_pos / 100);
+
+        if (current_pos <= 0 && !prev_was_0) {
             count++;
         }
+
+        current_pos %= 100;
+        if (current_pos < 0) {
+            current_pos += 100;
+        }
+        prev_was_0 = current_pos == 0;
     }
 
     printf("%d\n", count);
