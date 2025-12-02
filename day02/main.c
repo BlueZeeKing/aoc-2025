ff5d7bac0ba4e0c25c62dfdee64e3d54c1f38b06
Brayden Zee <github@blueish.dev>
2025-12-02 00:27:20 -0500

    Day 2, part 2
    I feel like this has a smarter solution, but I'm saving my brain cells
    for later.

--- day02/main.c
+++ day02/main.c
@@ -3,17 +3,40 @@
 #include <stdbool.h>
 
 int get_num_digits(long num) {
-    return ceil(log10(num));
+    return ceil(log10((double) num + 0.5));
 }
 
 bool is_invalid(long num) {
     int num_digits = get_num_digits(num);
 
-    if (num_digits % 2 == 1) {
-        return false;
+    for (int i = 1; i <= num_digits / 2; i++) {
+        if (num_digits % i != 0) {
+            continue;
+        }
+
+        long temp_num = num;
+        long mask = pow(10, i);
+
+        long segment = num % mask;
+        temp_num /= mask;
+
+        bool invalid = true;
+
+        for (int j = 1; j < num_digits / i; j++) {
+            if (temp_num % mask != segment) {
+                invalid = false;
+                break;
+            }
+
+            temp_num /= mask;
+        }
+
+        if (invalid) {
+            return true;
+        }
     }
 
-    return num / (long) pow(10, num_digits / 2) == num % (long) pow(10, num_digits / 2);
+    return false;
 }
 
 int main() {
