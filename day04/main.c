5f081827279ed43d61c1f214da96b30b7aa9c45c
Brayden Zee <github@blueish.dev>
2025-12-04 21:40:03 -0500

    Day 4, part 2

--- day04/main.c
+++ day04/main.c
@@ -2,6 +2,39 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int find_reachable(bool* occupied, bool *next_occupied, int width, int height) {
+    int count = 0;
+
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            int num_neighbors = 0;
+            for (int y_offset = -1; y_offset <= 1; y_offset++) {
+                for (int x_offset = -1; x_offset <= 1; x_offset++) {
+                    if (y_offset == 0 && x_offset == 0) {
+                        continue;
+                    }
+
+                    if (y + y_offset >= height || y + y_offset < 0 ||
+                        x + x_offset >= width || x + x_offset < 0) {
+                        continue;
+                    }
+
+                    if (occupied[(y + y_offset) * width + (x + x_offset)]) {
+                        num_neighbors++;
+                    }
+                }
+            }
+            if (num_neighbors < 4 && occupied[y * width + x]) {
+                next_occupied[y * width + x] = false;
+                count++;
+            }
+        }
+    }
+
+    return count;
+}
 
 int main() {
     char c;
@@ -39,40 +72,21 @@ int main() {
 
     assert(length == width * height);
 
+    bool *next_occupied = malloc(sizeof(bool) * length);
+    memcpy(next_occupied, occupied, sizeof(bool) * length);
 
+    int sum = 0;
+    int count = 1;
+    while ((count = find_reachable(occupied, next_occupied, width, height)) > 0) {
+        sum += count;
 
-    int count = 0;
+        bool *temp;
+        temp = occupied;
+        occupied = next_occupied;
+        next_occupied = temp;
 
-    for (int y = 0; y < height; y++) {
-        for (int x = 0; x < width; x++) {
-            int num_neighbors = 0;
-            for (int y_offset = -1; y_offset <= 1; y_offset++) {
-                for (int x_offset = -1; x_offset <= 1; x_offset++) {
-                    if (y_offset == 0 && x_offset == 0) {
-                        continue;
-                    }
-
-                    if (y + y_offset >= height || y + y_offset < 0 ||
-                        x + x_offset >= width || x + x_offset < 0) {
-                        continue;
-                    }
-
-                    if (occupied[(y + y_offset) * width + (x + x_offset)]) {
-                        num_neighbors++;
-                    }
-                }
-            }
-            if (num_neighbors < 4 && occupied[y * width + x]) {
-                printf("x");
-                count++;
-            } else if (occupied[y * width + x]) {
-                printf("@");
-            } else {
-                printf(".");
-            }
-        }
-        printf("\n");
+        memcpy(next_occupied, occupied, sizeof(bool) * length);
     }
 
-    printf("%d\n", count);
+    printf("%d\n", sum);
 }
